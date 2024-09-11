d739329cd672c415cca7248c8e96a2855a9e74a7
Brokn_Apples <trentons708@gmail.com>
2024-09-11 15:57:41 -0500

    Finished program, add quality of life features next

--- version1/combine_folders.cpp
+++ version1/combine_folders.cpp
@@ -9,27 +9,62 @@ bool IsProperFormat(const std::string& str, const int max_length);
 
 std::vector<std::filesystem::path> PrintAndChangeOrder(std::vector<std::filesystem::path>& files);
 
-void CombineAndRename(std::vector<std::filesystem::path>& ordering_list);
+bool CombineAndRename(std::vector<std::filesystem::path>& ordering_list);
 
 int main() {
   std::filesystem::path directory = std::filesystem::current_path();
+  const std::string kProgramFilename = "combine_folders.exe";
+  const int kProgramFilenameLength = kProgramFilename.length();
 
   std::vector<std::filesystem::path> files;
   // search directory and add to list
   for (const auto& file : std::filesystem::directory_iterator(directory)) {
     // do not include program file in list
-    if (file.path() == "combine_folders.exe") continue;
+    std::string str = file.path().string();
+    int temp_length = str.length();
+
+    bool isProgramFile = true;
+    for (int i = 0; i < kProgramFilenameLength; i++) {
+      if (str[(temp_length - kProgramFilenameLength) + i] != kProgramFilename[i]) isProgramFile = false;
+    }
+
+    // don't add program file to list
+    if (isProgramFile) continue;
     
     files.push_back(file.path());
   }
+
   std::vector<std::filesystem::path> ordering_list;
-  ordering_list = PrintAndChangeOrder(files);
+  bool is_valid = true;
+  do {
+    // Get files to include
+    ordering_list = PrintAndChangeOrder(files);
+    std::cout << std::endl;
 
-  for (auto& file : ordering_list) {
-    std::cout << file << "\n";
+    // List files and ask to confirm order
+    std::cout << "File order: \n";
+    int idx = 0;
+    for (auto& file : ordering_list) {
+      std::cout << idx << " - " << file << "\n";
+      idx++;
+    }
+    std::cout << "Press '/' and press ENTER if this is correct, type 'any other character' and press ENTER to go back: ";
+    std::string input = "";
+    std::cin >> input;
+    std::cout << std::endl;
+
+    if (input != "0" && input.length() > 0) is_valid = false;
+  } while (is_valid);
+
+  bool ret = CombineAndRename(ordering_list);
+
+  if (ret) {
+    std::cout << "\nFiles Successfully renamed." << std::endl;
+
+    std::cout << "Press 'any key' and press ENTER to close: ";
+    std::string input;
+    std::cin >> input;
   }
-
-  CombineAndRename(ordering_list);
 
   return 0;
 }
@@ -41,6 +76,8 @@ int main() {
   checks if the string meets all conditions
 */
 bool IsProperFormat(const std::string& str, const int max_length) {
+  if (str[0] == '/' && str.length() == 1) return true;
+
   const int kLength = str.length();
   int num_of_nums = 0;
   int curr_val = 0;
@@ -48,7 +85,7 @@ bool IsProperFormat(const std::string& str, const int max_length) {
 
   for (int i = 0; i < kLength; i++) {
     if (num_of_nums > max_length) {
-      std::cout << "Too many numbers entered." << std::endl;
+      std::cout << "Too many numbers entered.\n" << std::endl;
       return false;
     }
 
@@ -67,7 +104,7 @@ bool IsProperFormat(const std::string& str, const int max_length) {
         else {
           // if number has already been entered
           if (seen.find(curr_val) != seen.end()) {
-            std::cout << "Do not enter a number more than once." << std::endl;
+            std::cout << "Do not enter a number more than once.\n" << std::endl;
             return false;
           }
 
@@ -77,18 +114,16 @@ bool IsProperFormat(const std::string& str, const int max_length) {
       }
       
       if (curr_val > max_length) {
-        std::cout << curr_val << " is not the within range(0 - " << max_length << ")." << std::endl;
+        std::cout << curr_val << " is not the within range(0 - " << max_length << ").\n" << std::endl;
         return false;
       }
 
     } else if (str[i] != ' ') {
       // not a digit or space character, unexpected character entered
-      std::cout << "Improper format." << std::endl;
+      std::cout << "Improper format.\n" << std::endl;
       return false;
     }
   }
-
-  std::cout << "returning true: " << curr_val << std::endl;
 
   return true;
 }
@@ -101,21 +136,27 @@ bool IsProperFormat(const std::string& str, const int max_length) {
   the order they wish to combine them in
 */
 std::vector<std::filesystem::path> PrintAndChangeOrder(std::vector<std::filesystem::path>& files) {
-  // list all files
-  int idx = 0;
-  for (auto& file : files) {
-    std::cout << idx << " - " << file << "\n";
-    idx++;
-  }
-
   // Prompt user for order of files
-  std::string order;
+  std::string order = "";
   const int kLength = files.size();
   do {
-    std::cout << "Enter the order of files using spaces(e.g. 0 1 6 2 5 3 4),"
-              << " if nothing is entered, order is assumed to be correct: ";
+    // List all files
+    std::cout << "Files in directory: " << std::endl;
+    int idx = 0;
+    for (auto& file : files) {
+      std::cout << idx << " - " << file << "\n";
+      idx++;
+    }
+    
+    // Prompt user for input
+    std::cout << "Enter the order of files using spaces(e.g. '0 1 6 2 5 3 4');\n"
+              << "type '/' if the order is correct and all files should be included: ";
+    std::cin >> std::ws;
     std::getline(std::cin, order);
-  } while(!IsProperFormat(order, kLength));
+  } while(!IsProperFormat(order, (kLength - 1)));
+
+  // If string is empty, return original list
+  if (order.length() == 1) return files;
   
 
   // Parse files into ordering list
@@ -146,38 +187,57 @@ std::vector<std::filesystem::path> PrintAndChangeOrder(std::vector<std::filesyst
   takes a list containing the order of files to combine and
   creates a folder that contains the combined files
 */
-void CombineAndRename(std::vector<std::filesystem::path>& ordering_list) {
+bool CombineAndRename(std::vector<std::filesystem::path>& ordering_list) {
   // rename
   // use filesystem to get new list for each element ordering_list
   // then use names to rename...
   const int kListSize = ordering_list.size();
   std::string destination_folder = ordering_list[0].string();
 
-
   // get length and find smallest prefix of 0's to use
-  std::string last_idx = "1";
   int length = 0;
   
-  for (const auto& curr_folder : ordering_list) {
+    for (const auto& curr_folder : ordering_list) {
     for (const auto& file : std::filesystem::directory_iterator(curr_folder)) {
       length++;
     }
   }
 
-  int num_digits = 0;
-  while(length > 9) {
+  // find how many leading zeroes there will be, e.g. 001 || 000001
+  // only check to tens place to account for the guaranteed ones place
+  int leading_zeroes = 0;
+  while(length > 99) {
     length /= 10;
-    num_digits++;
+    leading_zeroes++;
   }
 
+  int idx_num = 1;
   for (const auto& curr_folder : ordering_list) {
     for (const auto& file : std::filesystem::directory_iterator(curr_folder)) {
-      const char* old_filename = "";
-      old_filename = file.path().string().c_str();
+      std::string old_filename = file.path().string().c_str();
+      std::string file_extension = file.path().extension().string();
 
-      const char* new_filename = "TODO: add renaming method";
+      std::string zeroes = "";
+      for (int i = 0; i < leading_zeroes; i++) {
+        zeroes += '0';
+      }
 
-      // rename(old_filename, new_filename);
+      std::string new_filename = ordering_list[0].string() + "\\" + zeroes
+                                 + std::to_string(idx_num) + file_extension;
+      idx_num++;
+
+      std::cout << "Old filename: " << old_filename << "\nNew filename: " << new_filename << "\n";
+      rename(old_filename.c_str(), new_filename.c_str());
+    }
+
+    // Delete empty folder if its not the first in the ordering_list
+    if (curr_folder != ordering_list[0]) {
+      std::error_code error_code;
+      if (!std::filesystem::remove(curr_folder, error_code)) {
+        std::cout << error_code.message() << std::endl;
+      }
     }
   }
+
+  return true;
 }
