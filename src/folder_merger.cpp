c59ad1cbc8f9c00e61a384c89770916cd93e8c36
Brokn_Apples <trentons708@gmail.com>
2024-10-24 15:24:13 -0500

    24-10-2024 update 1: added createBackup function

--- src/folder_merger.cpp
+++ src/folder_merger.cpp
@@ -4,17 +4,35 @@
 *********************************** PRIVATE ***********************************
 ******************************************************************************/ 
 
-// Check user input
+/**
+ * @brief Check if a file is a valid file to add to an ordering_list
+ *
+ * @param file file to check
+ * 
+ * @return true if success ; false if error
+ */
 bool FolderMerger::isValidEntry(std::filesystem::path file) {
   // if file is in the exclude list, return false
   if (m_exclude_list.find(file.filename()) != m_exclude_list.end()) {
     std::cout << file.filename() << " is an excluded filename. Skipping.\n";
     return false;
   }
+  else if (!std::filesystem::is_directory(file)) {
+    std::cout << file.filename() << " is not a directory, cannot merge this file. Skipping.\n";
+    return false;
+  }
 
   return true;
 }
 
+/**
+ * @brief Check if a string containing the order of files is in the proper format
+ *
+ * @param str string to check
+ * @param max_length maximum integer value possible for a valid string
+ * 
+ * @return true if success ; false if error
+ */
 bool FolderMerger::isProperFormat(std::string_view str, const int max_length) {
   const int kLength = str.length();
   int num_of_nums = 0;
@@ -67,7 +85,13 @@ bool FolderMerger::isProperFormat(std::string_view str, const int max_length) {
   return true;
 }
 
-// General use
+/**
+ * @brief Get a list of every child in a directory
+ *
+ * @param directory directory to get children from
+ * 
+ * @return std::vector<std::filesystem::path> containing path elements
+ */
 std::vector<std::filesystem::path> FolderMerger::getDirectoryEntries(std::filesystem::path directory) {
   std::vector<std::filesystem::path> ret;
   for (const auto& file : std::filesystem::directory_iterator(directory)) {
@@ -77,6 +101,11 @@ std::vector<std::filesystem::path> FolderMerger::getDirectoryEntries(std::filesy
   return ret;
 }
 
+/**
+ * @brief Print all entries in a vector containing filesystem paths
+ *
+ * @param directory holds a list of path variables to print
+ */
 void FolderMerger::printEntries(std::vector<std::filesystem::path>& directory) {
   for (int i = 0; i < directory.size(); i++) {
     std::cout << i << " - " << directory[i] << "\n";
@@ -84,8 +113,13 @@ void FolderMerger::printEntries(std::vector<std::filesystem::path>& directory) {
   std::cout << std::endl;
 }
 
-// Main working functions
-
+/**
+ * @brief Get a list in the correct order of the folders of the elements to merge
+ *
+ * @param entries a list containing all of the entries in the current directory
+ * 
+ * @return std::vector<std::filesystem::path> containing the elements in the correct order
+ */
 std::vector<std::filesystem::path> FolderMerger::getOrderingList(std::vector<std::filesystem::path>& entries) {
   // Prompt user for order of files
   std::string input;
@@ -136,54 +170,155 @@ std::vector<std::filesystem::path> FolderMerger::getOrderingList(std::vector<std
   return ordering_list;
 }
 
-
-bool FolderMerger::createBackup(std::vector<std::filesystem::path>& ordering_list, std::filesystem::path& curr_directory) {
-
-}
-
-void FolderMerger::getValidBackupPath() {
-  while(false) {
-
+/**
+ * @brief Checks if a path contains a valid path to some folder
+ *
+ * @param ordering_list the vector of paths to copy
+ * @param check_directory is the parth param a directory
+ * 
+ * @return true if success ; false if error occurred
+ */
+bool FolderMerger::isValidPath(std::filesystem::path path, bool check_directory) {
+  if (std::filesystem::exists(path)) {
+    if (!check_directory) {
+      return false;
+    }
+    else if (check_directory && std::filesystem::is_directory(path)) {
+      return false;
+    }
   }
-}
 
-bool FolderMerger::appendIndex(std::string_view str) {
   return true;
 }
 
-void FolderMerger::getValidIndexPath() {
-  while(false) {
+/**
+ * @brief Creates a backup folder containing all of a std::vector<std::filesystem::path> variable's original content
+ *
+ * @param ordering_list the vector of paths to copy
+ * 
+ * @return true if success ; false if error occurred
+ */
+bool FolderMerger::createBackup(std::vector<std::filesystem::path>& ordering_list) {
+  std::filesystem::path backup_path = DEFAULT_BACKUP_PATH;
 
+  // Check if path is a valid backup directory name
+  if (!isValidPath(backup_path, true)) {
+    backup_path = getValidBackupPath();
   }
+
+  std::filesystem::create_directory(backup_path);
+  // copy all data to backup  
+  for (auto& file : ordering_list) {
+    // new_filename will be : path-to-parent/backup_path/file.extension
+    std::filesystem::path new_filename = (m_main_directory / backup_path / "/" / file);
+    std::filesystem::copy(file, new_filename);
+  }
+
+  return true;
+}
+
+/**
+ * @brief Get a valid name for a backup directory
+ *
+ * @return std::filesystem::path which holds the valid backup path name
+ */
+std::filesystem::path FolderMerger::getValidBackupPath() {
+  std::string input;
+  do {
+    std::cout << "Enter a valid backup folder name: ";
+    
+    std::cin >> std::noskipws;
+    std::getline(std::cin, input);
+    // check if file exists, or if there is not a directory with that name
+  } while (!isValidPath(input, true) && input != "");
+
+  return input;
+}
+
+/**
+ * @brief create a file that will contain the index of all of the merged folder's contents BUT do not add anything to it yet
+ * 
+ * @return true if success ; false if error
+ */
+bool FolderMerger::createIndex() {
+  std::filesystem::path backup_path = DEFAULT_BACKUP_PATH;
+
+  // Check if path is a valid backup directory name
+  if (!isValidPath(backup_path, true)) {
+    backup_path = getValidBackupPath();
+  }
+
+  std::filesystem::create_directory(backup_path);
+
+  return true;
+}
+
+bool FolderMerger::appendIndex(std::string_view str) {
+  // create index some time earlier,
+  return true;
+}
+
+/**
+ * @brief Get a valid name for a backup directory
+ *
+ * @return std::filesystem::path which holds the valid backup path name
+ */
+std::filesystem::path FolderMerger::getValidBackupPath() {
+  std::string input;
+  do {
+    std::cout << "Enter a valid backup folder name: ";
+    
+    std::cin >> std::noskipws;
+    std::getline(std::cin, input);
+    // check if file exists, or if there is not a directory with that name
+  } while (!isValidPath(input, true) && input != "");
+
+  return input;
 }
 
 /******************************************************************************
 *********************************** PUBLIC ************************************
 ******************************************************************************/ 
 
+/**
+ * @brief The only constructor for a FolderMerger instnace
+ *
+ * @param main_directory path to the directory that will will merge folders
+ */
 FolderMerger::FolderMerger(std::filesystem::path main_directory)
   : m_main_directory(main_directory) {
-  m_name = "FolderMerger.exe";
+  m_name = "fmerge.exe";
 }
 
+/**
+ * @brief Get a list of filenames to exclude from merging
+ */
 void FolderMerger::getCustomExcludes() {
-  std::cout << "Enter files that should be excluded from parsing(enter " + QUIT_FLAG + " to end input): ";
+  std::cout << "Enter files that should be excluded from parsing(enter " + QUIT_FLAG + " to end input): " << std::endl;
   std::string input = "";
   while (input != QUIT_FLAG) {
     if ((input != QUIT_FLAG) && (input != "")) {
       m_exclude_list[input]++;
     }
+    std::cin >> std::noskipws;
     std::getline(std::cin, input);
   }
 }
 
-/* Optional: Default exclude list is empty */
+/**
+ * @brief Add a list of filenames to exclude from merging, Optional: Default exclude list is empty
+ *
+ * @param exclude_list list of files to exclude
+ */
 void FolderMerger::addToExcludeList(const std::vector<std::filesystem::path>& exclude_list) {
   for (auto& file : exclude_list) {
     m_exclude_list[file]++;
   }
 }
 
+/**
+ * @brief run a FolderMerger instance
+ */
 void FolderMerger::run() {
   std::vector<std::filesystem::path> main_dir_files = getDirectoryEntries(m_main_directory);
 
@@ -195,9 +330,8 @@ void FolderMerger::run() {
 
     std::cout << "\nCurrent Order: " << std::endl;
     printEntries(ordering_list);
-    std::cout << "Press 'any key' and press 'ENTER' to go back, "
-              << "or enter nothing and press 'ENTER' if this is the correct order: "
-              << std::endl;
+    std::cout << "Press 'any key' and press 'ENTER' to go back, or enter "
+              << "nothing and press 'ENTER' if this is the correct order: " << std::endl;
     std::string input = "";
     std::cin >> input;
     std::cin.ignore();
@@ -210,5 +344,47 @@ void FolderMerger::run() {
     }
   } while(loop);
   
-  // TODO: Create backup, index and rename files
+  // Create backup, index, and rename files
+  do {
+    std::cout << "Enter a merge method(Enter only ONE):\n"
+              << "FLAGNAME  |  RESULT\n"
+              << "'ENTER':     Create a Backup and an Index\n"
+              << "'-b':        Do not create a Backup, but create an Index\n"
+              << "'-i':        Do not create a Index, but create an Backup\n"
+              << "'-c':        Do not create a Backup or an Index\n"
+              << "'-q':        Quit Program\n"
+              << std::endl;
+
+    std::string merge_method = "";
+    std::cin >> merge_method;
+
+    std::remove_if(merge_method.begin(), merge_method.end(), isspace);
+
+    loop = false;
+    switch(merge_method[1]) {
+      case '\0': // Create Backup and Index
+        createBackup(ordering_list);
+        createIndex();
+        break;
+
+      case 'b': // No Backup
+        createIndex();
+        break;
+
+      case 'i': // No Index
+        createBackup(ordering_list);
+        break;
+
+      case 'c': // No Backup or Index
+        break;
+
+      case 'q': // Quit Program
+        std::cout << "Quitting program." << std::endl;
+        return;
+
+      default:
+        std::cout << "ERROR: incorrect entry: " << merge_method << std::endl;
+        loop = true;
+    }
+  } while (loop);
 }
--- src/folder_merger.hpp
+++ src/folder_merger.hpp
@@ -2,18 +2,22 @@
 #define FOLDER_MERGER_HPP
 
 #include <iostream>
+#include <string>
+#include <string_view>
 #include <vector>
 #include <unordered_map>
 #include <filesystem>
-#include <string_view>
+#include <algorithm>
 
 class FolderMerger {
  private:
- // flags
-  std::string QUIT_FLAG = "-q";
-  std::string NO_BACKUP_FLAG = "-b";
-  std::string NO_INDEX_FLAG = "-i";
-  std::string NO_BACKUP_OR_INDEX_FLAG = "-c";
+  // flags
+  const std::string QUIT_FLAG = "-q";
+  const std::string NO_BACKUP_FLAG = "-b";
+  const std::string NO_INDEX_FLAG = "-i";
+  const std::string NO_BACKUP_OR_INDEX_FLAG = "-c";
+  const std::filesystem::path DEFAULT_BACKUP_PATH = "Backup";
+  const std::filesystem::path DEFAULT_INDEX_PATH = "Index";
 
   // vars
   std::unordered_map<std::filesystem::path, int> m_exclude_list; // List of filenames to exclude
@@ -32,12 +36,13 @@ class FolderMerger {
 
   // Main working functions
   std::vector<std::filesystem::path> getOrderingList(std::vector<std::filesystem::path>& entries);
+  bool isValidPath(std::filesystem::path path, bool check_directory);
 
-  bool createBackup(std::vector<std::filesystem::path>& ordering_list, std::filesystem::path& curr_directory);
-  void getValidBackupPath();
+  bool createBackup(std::vector<std::filesystem::path>& ordering_list);
+  std::filesystem::path getValidBackupPath();
 
+  bool createIndex();
   bool appendIndex(std::string_view str);
-  void getValidIndexPath();
 
  public:
   FolderMerger(std::filesystem::path main_directory);
